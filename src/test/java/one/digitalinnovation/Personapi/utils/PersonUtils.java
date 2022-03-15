3b174349f37d4daeed4822780c5202eea100500a
GPMrks <guilhermepereiramarques@hotmail.com>
2022-03-15 00:54:10 -0300

    Developed Unit Tests for Create Person

---
+++ src/test/java/one/digitalinnovation/Personapi/utils/PersonUtils.java
@@ -0,0 +1,38 @@
+package one.digitalinnovation.Personapi.utils;
+
+import one.digitalinnovation.Personapi.dto.request.PersonDTO;
+import one.digitalinnovation.Personapi.entity.Person;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "Guilherme";
+    private static final String LAST_NAME = "Marques";
+    private static final String CPF_NUMBER = "124.210.796-75";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(1994, 06, 26);
+
+    public static PersonDTO createFakeDTO() {
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate("26-06-1994")
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity() {
+        return Person.builder()
+                .id(PERSON_ID)
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+    }
+
+}
